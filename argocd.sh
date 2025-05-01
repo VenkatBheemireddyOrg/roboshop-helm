3f4b84a63fcfec7392337e454a42b807c5beca4e
venkat <devops.bheemireddy@gmail.com>
2025-05-02 09:03:48 +1000

    Project Roboshop Ansible with Multi environment

--- argocd.sh
+++ argocd.sh
@@ -11,7 +11,7 @@ argocd account list &>/dev/null
 if [ $? -ne 0 ]; then
     argocd_ip=$(kubectl get svc -n argocd argocd-server -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
     argocd_password=$(kubectl get secret argocd-initial-admin-secret -n argocd -o=jsonpath='{.data.password}' | base64 --decode)
-    argocd login {argocd_ip} --insecure --username admin --password ${argocd_password}
+    argocd login ${argocd_ip} --insecure --username admin --password ${argocd_password}
 fi
 
 
