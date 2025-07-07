5a3a05426cf71e9ef29fad5d7adfebd09d83811b
venkat <devops.bheemireddy@gmail.com>
2025-07-07 18:06:51 +1000

    Latest Changes

--- argocd-auto.sh
+++ argocd-auto.sh
@@ -4,9 +4,10 @@ argocd_ip=$(kubectl get svc -n argocd argocd-server -o jsonpath='{.status.loadBa
 argocd_password=$(kubectl get secret argocd-initial-admin-secret -n argocd -o=jsonpath='{.data.password}' | base64 --decode)
 argocd login argocd-dev.azdevopsv82.online:443 --insecure --username admin --password ${argocd_password} --grpc-web
 
-for app_name in cart catalogue user shipping payment frontend; do
+for app_name in cart; do
 
 appImage=$(gh search commits --repo VenkatBheemireddyOrg/roboshop-$app_name "Latest Changes" --sort committer-date | head -1 | awk '{print $2}')
-argocd app create --upsert ${app_name} --repo https://github.com/VenkatBheemireddyOrg/roboshop-helm.git --dest-namespace default --dest-server https://kubernetes.default.svc --values env-dev/${app_name}.yaml  --path . --helm-set appImage=roboshopb82new.azurecr.io/roboshop-$app_name:$appImage --grpc-web
+argocd app create --upsert ${app_name} --repo https://github.com/VenkatBheemireddyOrg/roboshop-helm.git --dest-namespace default --dest-server https://kubernetes.default.svc --values env-dev/${app_name}.yaml  --path . --helm-set appImage=roboshopb82.azurecr.io/roboshop-$app_name:$appImage --grpc-web
 argocd app sync ${app_name} --grpc-web
+
 done
